8e75b224c2c04e2eb810ad6319da949c5efebe19
carovicfilip <carovic.filip@gmail.com>
2025-07-17 01:09:06 +0200

    Progress 3

---
+++ lib/core/constants/app_images.dart
@@ -0,0 +1,122 @@
+class AppAssets {
+  // Images
+  static const SPLASH = 'assets/images/splash.png';
+  static const NEWSPLASH = 'assets/images/new_splash.png';
+  static const paywall = 'assets/images/paywall_background.png';
+  static const uploadMediaGroup = 'assets/images/upload_media_group.png';
+  static const userDisabled = 'assets/images/user_disabled.png';
+  static const userVerified = 'assets/images/user_verified.png';
+
+  // Icons
+  static const LOGO_IC = 'assets/icons/logo-ic.png';
+  static const APPLE_IC = 'assets/icons/apple.svg';
+  static const takeImage = 'assets/icons/takeImage.svg';
+  static const checkSVG = 'assets/icons/checkSVG.svg';
+  static const cameraSVG = 'assets/icons/camera.svg';
+  static const micSVG = 'assets/icons/mic.svg';
+  static const liberarySVG = 'assets/icons/liberary.svg';
+  static const documentSVG = 'assets/icons/document.svg';
+  static const FB_IC = 'assets/icons/facebook.svg';
+  static const fileSVG = 'assets/icons/file.svg';
+  static const HOME_IC = 'assets/icons/home-ic.svg';
+  static const GOOGLE_IC = 'assets/icons/google-ic.svg';
+  static const BACK_IC = 'assets/icons/back-ic.svg';
+  static const CLOSE_IC = 'assets/icons/close-ic.svg';
+  static const HOME_TAB_IC = 'assets/icons/home-tab-ic.svg';
+  static const STATUS_IC = 'assets/icons/status-ic.svg';
+  static const TAG_IC = 'assets/icons/tag-ic.svg';
+  static const people = 'assets/icons/people.svg';
+  static const setting = 'assets/icons/setting.svg';
+  static const ADD_CAMERA_IC = 'assets/icons/add-camera-ic.svg';
+  static const searchSVG = 'assets/icons/search.svg';
+
+  static const CALENDAR_IC = 'assets/icons/calendar-ic.svg';
+  static const calender = 'assets/icons/calendar.svg';
+  static const LOCATION_IC = 'assets/icons/location-ic.svg';
+  static const GEOLOCATION = 'assets/icons/geolocation.svg';
+  static const blackPlusIC = 'assets/icons/black_plus_ic.svg';
+  static const PROFILE_IC = 'assets/icons/profile-ic.svg';
+  static const ARROW_BACK_IC = 'assets/icons/arrow-back-ic.svg';
+  static const arrowBack = 'assets/icons/arrow-back.png';
+  static const arrowBackSvg = 'assets/icons/arrow-back.svg';
+  static const addPlus = 'assets/icons/add-plus.png';
+  static const editPencilIc = 'assets/icons/edit_pencil_ic.svg';
+  static const edit = 'assets/icons/edit.svg';
+  static const newEdit = 'assets/icons/new_edit.svg';
+  static const userAvatar = 'assets/icons/userAvatar.svg';
+  static const shareSVG = 'assets/icons/share.svg';
+  static const moreHorizontalSVG = 'assets/icons/moreHZ.svg';
+  static const moreVerticalSVG = 'assets/icons/more_dots.svg';
+  static const pinnedSVG = 'assets/icons/pinned.svg';
+  static const locationMarkerSVG = 'assets/icons/location_marker.svg';
+  static const locationPinSVG = 'assets/icons/location-pin.svg';
+  static const locationPinIconSVG = 'assets/icons/location-pin-icon.svg';
+  static const successTickSVG = 'assets/icons/succes_tick.svg';
+  static const exportIconSvg = 'assets/icons/export_icon.svg';
+  static const backupIconSvg = 'assets/icons/backup_icon.svg';
+  static const IC_PASSWORD = 'assets/icons/ic_password.svg';
+  static const threeDots = 'assets/icons/three_dots.svg';
+  static const attachIcon = 'assets/icons/attach.svg';
+  static const filterIcon = 'assets/icons/filter.svg';
+  static const filterOffIcon = 'assets/icons/filter-off.svg';
+  static const square = 'assets/icons/square.svg';
+  static const imagePlus = 'assets/icons/image-plus.svg';
+  static const userCheck = 'assets/icons/user_check.svg';
+  static const shieldPerson = 'assets/icons/shield-person.svg';
+  static const germanFlag = 'assets/icons/flags/de.svg';
+  static const italianFlag = 'assets/icons/flags/it.svg';
+  static const frenchFlag = 'assets/icons/flags/fr.svg';
+  static const spanishFlag = 'assets/icons/flags/es.svg';
+  static const britishFlag = 'assets/icons/flags/gb.svg';
+  static const serbianFlag = 'assets/icons/flags/rs.svg';
+  static const bosnianFlag = 'assets/icons/flags/ba.svg';
+  static const montenegrinFlag = 'assets/icons/flags/me.svg';
+  static const albanianFlag = 'assets/icons/flags/al.svg';
+  static const trash = 'assets/icons/trash-alt.svg';
+  static const microphone = 'assets/icons/microphone.svg';
+  static const pause = 'assets/icons/pause-circle.svg';
+  static const chat = 'assets/icons/chat.svg';
+  static const chatDelete = 'assets/icons/chat-delete.svg';
+  static const chatIcon = 'assets/icons/chat_icon.svg';
+  static const reply = 'assets/icons/reply.svg';
+  static const time = 'assets/icons/time.svg';
+  static const members = 'assets/icons/members.svg';
+  static const chevronDown = 'assets/icons/chevron-down.svg';
+  static const sendAlt = 'assets/icons/send_alt.svg';
+  static const threadChat = 'assets/icons/thread.svg';
+  static const check = 'assets/icons/check.svg';
+  static const checkCircle = 'assets/icons/check-circle.svg';
+  static const userPlus = 'assets/icons/user-plus.svg';
+  static const eye = 'assets/icons/eye.svg';
+  static const notification = 'assets/icons/notification.svg';
+  static const notificationNew = 'assets/icons/notification_new.svg';
+
+  ///ONBOARDING
+  static const onboardingOne = 'assets/images/onboarding_one.png';
+  static const onboardingTwo = 'assets/images/onboarding_two.png';
+  static const onboardingThree = 'assets/images/onboarding_three.png';
+  static const onboardingFour = 'assets/images/onboarding_four.png';
+  static const onboardingFive = 'assets/images/onboarding_five.png';
+
+  ///BOTTOM NAVIGATION BAR
+  static const home = 'assets/icons/home.svg';
+  static const reports = 'assets/icons/reports.svg';
+  static const teams = 'assets/icons/teams.svg';
+  static const upload = 'assets/icons/upload.svg';
+  static const uploadMedia = 'assets/icons/upload-media.svg';
+
+  ///USER SETTINGS
+  static const user = 'assets/icons/user.svg';
+  static const company = 'assets/icons/company.svg';
+  static const key = 'assets/icons/key.svg';
+  static const flag = 'assets/icons/flag.svg';
+  static const unarchive = 'assets/icons/unarchive.svg';
+  static const cloud = 'assets/icons/cloud.svg';
+  static const fileCheck = 'assets/icons/file-check.svg';
+  static const coins = 'assets/icons/coins.svg';
+  static const logout = 'assets/icons/logout.svg';
+  static const userCancel = 'assets/icons/user-cancel.svg';
+  static const infoCircle = 'assets/icons/info-circle.svg';
+  static const cloudDone = 'assets/icons/cloud-done.svg';
+  static const helpTooltip = 'assets/icons/help_tooltip.svg';
+}
